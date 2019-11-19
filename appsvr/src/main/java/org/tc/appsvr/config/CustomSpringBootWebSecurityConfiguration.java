62fc50355a774660f6ad78c5063660c89fcc7f3b
unknown <tancan@wasion.cn>
2019-11-19 13:35:42 +0800

    change the response from redirect to JSON data when requesting api without auth

--- appsvr/src/main/java/org/tc/appsvr/config/CustomSpringBootWebSecurityConfiguration.java
+++ appsvr/src/main/java/org/tc/appsvr/config/CustomSpringBootWebSecurityConfiguration.java
@@ -1,15 +1,22 @@
 package org.tc.appsvr.config;
 
+import cn.hutool.http.HttpStatus;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.tc.appsvr.entity.Rest;
+import org.tc.appsvr.entity.RestBody;
 import org.tc.appsvr.filter.JsonLoginPostProcessor;
 import org.tc.appsvr.filter.LoginPostProcessor;
 import org.tc.appsvr.filter.PreLoginFilter;
@@ -17,6 +24,11 @@ import org.tc.appsvr.handler.CustomLogoutHandler;
 import org.tc.appsvr.handler.CustomLogoutSuccessHandler;
 
 import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Collection;
 
 /**
@@ -84,7 +96,24 @@ public class CustomSpringBootWebSecurityConfiguration {
 //                    .addFilterBefore(preLoginFilter, UsernamePasswordAuthenticationFilter.class)
                     // 登录
                     .formLogin().loginProcessingUrl(LOGIN_PROCESSING_URL).successForwardUrl("/login/success").failureForwardUrl("/login/failure")
-                    .and().logout().addLogoutHandler(new CustomLogoutHandler()).logoutSuccessHandler(new CustomLogoutSuccessHandler());
+                    .and().logout().addLogoutHandler(new CustomLogoutHandler()).logoutSuccessHandler(new CustomLogoutSuccessHandler())
+                    .and().exceptionHandling().authenticationEntryPoint(new AuthenticationEntryPoint() {
+                        // 没有权限直接返回 JSON，不再做重定向操作
+                        @Override
+                        public void commence(HttpServletRequest req, HttpServletResponse resp, AuthenticationException e) throws IOException, ServletException {
+                            resp.setContentType("application/json;charset=utf-8");
+                            PrintWriter out = resp.getWriter();
+                            Rest respBean = RestBody.failure(HttpStatus.HTTP_UNAUTHORIZED, "访问失败");
+//                            RespBean respBean = RespBean.error("访问失败!");
+                            if (e instanceof InsufficientAuthenticationException) {
+                                respBean.setMsg("请求失败，请联系管理员!");
+                            }
+                            out.write(new ObjectMapper().writeValueAsString(respBean));
+                            out.flush();
+                            out.close();
+                        }
+            })
+            ;
 
         }
     }
